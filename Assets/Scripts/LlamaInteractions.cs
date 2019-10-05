c3f8854efdfec4c02652da2a2fd4fabb49408456
WubbaDuck <kd8gby@gmail.com>
2019-10-05 01:59:55 -0400

    Added a timer to the harvesting.

--- Assets/Scripts/LlamaInteractions.cs
+++ Assets/Scripts/LlamaInteractions.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class LlamaInteractions : MonoBehaviour
 {
     public Collider2D interactableCollider;
-    private bool interactTrigger;
 
-    private string[] pickups = {
-        "Tree",
-        "Stick",
-        "Rock",
-        "Stone",
-        "Grass",
-        "Hay",
-        "Egg"
-    };
+    private bool interactTrigger = false;
+    private bool harvestingTimerStarted = false;
+    private bool harvestingKeyHeld = false;
+    private float startTime = 0f;
+    private float timer = 0f;
+    private float harvestingTime = 2.0f;
+    private GameObject thingToHarvest;
+    private string successfulHarvestOutputName = "";
+    private int successfulHarvestOutputQuantity = 0;
+    InventoryManager im;
+
+    void Start()
+    {
+        im = gameObject.transform.GetComponent<InventoryManager>();
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.E))
         {
             interactTrigger = true;
+            timer += Time.deltaTime;
         }
         else
         {
             interactTrigger = false;
+
+            ResetHarvesting();
+        }
+
+        if (harvestingTimerStarted && timer > (startTime + harvestingTime))
+        {
+            for (int i = 0; i < successfulHarvestOutputQuantity; i++)
+            {
+                im.AddItemToInventory(successfulHarvestOutputName);
+            }
+
+            Destroy(thingToHarvest);
+            harvestingTimerStarted = false;
         }
     }
 
@@ -37,44 +56,55 @@ public class LlamaInteractions : MonoBehaviour
 
             if (interactableCollider.IsTouchingLayers(LayerMask.GetMask("Interactables")))
             {
-                InventoryManager im = gameObject.transform.GetComponent<InventoryManager>();
-
                 switch (other.name)
                 {
                     case "Tree":
-                        im.AddItemToInventory("Sticks");
-                        im.AddItemToInventory("Sticks");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Sticks", 2, 1.5f);
                         break;
                     case "Stick":
-                        im.AddItemToInventory("Sticks");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Sticks", 1, 0f);
                         break;
                     case "Rock":
-                        im.AddItemToInventory("Stones");
-                        im.AddItemToInventory("Stones");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Stones", 2, 3f);
                         break;
                     case "Stone":
-                        im.AddItemToInventory("Stones");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Stones", 1, 0f);
                         break;
                     case "Grass":
-                        im.AddItemToInventory("Grass");
-                        Destroy(other.gameObject);
-                        break;
-                    case "Hay":
-                        im.AddItemToInventory("Grass");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Grass", 1, 1);
                         break;
                     case "Egg":
-                        im.AddItemToInventory("Eggs");
-                        Destroy(other.gameObject);
+                        Harvest(other.gameObject, "Eggs", 1, 0f);
                         break;
                     default:
                         break;
                 }
             }
         }
+    }
+
+    void Harvest(GameObject obj, string targetName, int targetQuantity, float timeToHarvest)
+    {
+        if (!harvestingTimerStarted)
+        {
+            harvestingTimerStarted = true;
+            startTime = Time.time;
+            timer = startTime;
+
+            thingToHarvest = obj;
+            successfulHarvestOutputName = targetName;
+            successfulHarvestOutputQuantity = targetQuantity;
+            harvestingTime = timeToHarvest;
+        }
+    }
+
+    void ResetHarvesting()
+    {
+        harvestingTimerStarted = false;
+        startTime = 0f;
+        timer = 0f;
+        harvestingTime = 0f;
+        successfulHarvestOutputName = "";
+        successfulHarvestOutputQuantity = 0;
     }
 }
