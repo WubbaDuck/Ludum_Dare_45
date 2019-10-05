2889521b189dd6e02ff4a86e48f81cea82110aba
WubbaDuck <kd8gby@gmail.com>
2019-10-05 10:51:36 -0400

    Added dynamic inventory listing. Resolves #15

---
+++ Assets/Scripts/InventoryDisplayController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplayController : MonoBehaviour
+{
+    float baseX = 2f;
+    float baseY = -9f;
+    float yStep = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Disable all inventory items and set them to the inventory origin position
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+            transform.GetChild(i).gameObject.transform.localPosition = new Vector2(baseX, baseY);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int enabledCount = 0;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject thisChild = transform.GetChild(i).gameObject;
+
+            if (int.Parse(thisChild.transform.Find("Count").GetComponent<Text>().text) > 0)
+            {
+                thisChild.SetActive(true);
+                thisChild.transform.localPosition = new Vector2(baseX, baseY - enabledCount * yStep + GetComponentInParent<RectTransform>().rect.height / 2);
+                enabledCount++;
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.transform.localPosition = new Vector2(baseX, baseY);
+                thisChild.SetActive(false);
+            }
+        }
+    }
+}
